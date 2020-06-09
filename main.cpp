598404d8623dafed8238a9e4e4943e1de5c858c7
Bui Duc Tuan <antivenomff@gmail.com>
2020-06-09 13:51:22 +0400

    Lab 9

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "sdt.h"
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a,b;
+	cout<<"Enter A and B : ";
+	cin>> a >> b;
+	cout<<"A + B = "<<a+b<<"\n";
+	return 0;
+}
